34cedc86b8097630aa699044024da7237f6995e0
Salompablo <pablosalompita@gmail.com>
2021-11-25 16:28:17 +0100

    Create #Iteración3

---
+++ Iteración3.js
@@ -0,0 +1,29 @@
+//1.1
+
+var a = 10;
+var b = 5;
+var total = a * b;
+alert(total);
+
+//1.2
+var c = 10;
+var d = 2;
+var total2 = c / d;
+alert(total2);
+
+//1.3
+var e = 15;
+var f = 9;
+var total3 = e / f;
+alert(total3);
+
+//1.4
+var y = 10;
+var z = 5;
+var x = y + z;
+
+//1.5
+var h = 10;
+var i = 5;
+var j = h * i;
+
